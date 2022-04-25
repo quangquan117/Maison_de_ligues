983221120b98826cf28477ad098e84b42131605e
leo langlois <leo.langlois2@gmail.com>
2022-04-25 16:48:55 +0200

    change to sport

--- formulaire.php
+++ formulaire.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MLLS</title>
-    <link href="./ico/halo.ico" rel="icon">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="./css/reset.css">
-    <link rel="stylesheet" href="./css/style.css">
-</head>
+<?php
+    include_once "./src/header.inc.php";
+?>
 <body>
     <header>
         <img src="./asset/halo_championship.png" alt="image filmonde">
---
+++ index.php
@@ -0,0 +1,331 @@
+<?php
+    include_once "./src/header.inc.php";
+?>
+<body>
+    <header>
+        <img src="./asset/halo_championship.png" alt="image filmonde">
+        <h1>Maison des ligue tous les sports</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Prêt à la compétition? Remplissez le formulaire proposé dans cette page</h2>
+            <p>
+                Tous les mois profitez de toutes les nouvelle opportunités. A partir du mois
+                prochain on vous propose tous les séance de sports sur vos support préférés.
+            </p>
+        </section>
+        <ul class="grid-picture-large">
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall"
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+            <li 
+            data-image="./asset/nightfall.jpg" 
+            data-title="Nightfall" 
+            data-description="c'est un film épatant, entre action et émotion..." 
+            data-dates="02/01/2020">
+                <figure>
+                    <img src="./asset/nightfall.jpg" alt="">
+                        <figcaption>
+                            <h2>
+                                <i class="material-icons" aria-hidden="true">
+                                    pages
+                                </i>
+                                Agrandir
+                                    
+                            </h2>
+                        </figcaption>
+                    
+                </figure>
+            </li>
+        </ul>
+        <nav>
+            <ul>
+                <li><a href="./formulaire.php">Cliquez ici pour commencer</a></li>
+            </ul>
+        </nav>
+    </main>
+    <footer>
+        <p>&copy; CINEMA STREMING - 2022</p>
+    </footer>
+    <!-- modale -->
+    <div class="parent-modale" role="dialog">
+        <figure class="modale">
+            <button aria-label="closed">
+                <span class="material-icons">clear</span>
+            </button>
+            <img src="https://via.placeholder.com/500" alt="picture">
+            <figcaption class="desc">
+                <h3>title</h3>
+                <p>
+                   
+                </p>
+                <time>Years : </time>
+            </figcaption>
+        </figure>
+    </div>
+    <script src="./js/agrandit.js"></script>
+</body>
+</html>
