2b15d57cbbf724218349d27b736e7569f9422fa8
leo langlois <leo.langlois2@gmail.com>
2022-06-17 11:36:56 +0200

    chang password

--- js/form.js
+++ js/form.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         else{
             warning.style.display="block";
             warning.classList.add("success");
+            warning.textContent ="Votre inscription est reussie"
             sessionStorage.setItem("prenom", User.nom);
             localStorage.setItem("nom", User.prenom);
             localStorage.setItem("email", User.email);
